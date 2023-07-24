d1c77807440cea1929232a6ee3ae699781de69a3
NakagawaKatsuyuki <katsuengineer2023@gmail.com>
2023-07-24 10:01:29 +0900

    userのマイグレーションファイル更新
    古い方のファイルを残し、そこにマイグレーションの内容を記述しました。
    
    中川

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -12,13 +13,18 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->bigIncrements('id');
+            $table->string('name', 50)->nullable(false);
+            $table->string('email', 100)->nullable(false)->unique();
+            $table->timestamp('email_verified_at')->nullable(true);
+            $table->string('password', 100)->nullable(false);
+            $table->integer('authority')->default(1);
+            $table->string('remember_token', 100)->nullable(true);
+            $table->boolean('is_show')->default(true);
+            // created_atをNOT NULLに設定するため、個別に記載
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP')); // デフォルト値を明示的に指定
+            $table->timestamp('updated_at')->nullable(true);
+            $table->softDeletes('deleted_at')->nullable(true);
         });
     }
 
