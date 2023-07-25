64c5c7639bd96f1a79a988402d6b6ad9fbbd9a48
yamakawa00 <139103368+yamakawa00@users.noreply.github.com>
2023-07-25 15:30:15 +0900

    ランキング
    コースごとに表示を変更出来るようにした。
    
    山川

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -23,12 +23,13 @@ class RankingController extends Controller
         // コース(難易度)別に各言語カテゴリーのランキングTOP10を表示させる(デフォルトはのんびりコース)
 
         // モデルからデータを取得
-        $items = Score::orderBy('Score', 'desc')
-            ->take(10)
+        $items = Score::where('is_show', 1)
+            ->orderBy('Score', 'desc')
             ->get();
 
 
         $data = [
+            'level_id' => '',
             'items' => $items,
         ];
 
