888cdbe2b4b72e132c37e7a5534df92259067c49
NakagawaKatsuyuki <katsuengineer2023@gmail.com>
2023-08-03 15:44:57 +0900

    知っトク記事詳細画面修正
    知っトク記事の詳細ページをフロント班からもらったHTMLで置き換えた。
    サイドバーは未だ中途半端な状態。
    
    中川

--- resources/views/fronts/knowhow_details.blade.php
+++ resources/views/fronts/knowhow_details.blade.php
@@ -7,31 +7,66 @@
 @section('title','知っトク記事詳細')
 
 @section('pageCss')
+    <link rel="stylesheet" href="../assets/css/shittokudetails.css">
+@endsection
 
 @section('key_visual')
 
 {{-- メインコンテンツの内容 --}}
-@section('maincontents')
-    <h1>知っトク記事詳細</h1>
-    <p class="margin"><a href="{{ route('knowhow') }}">知っトク情報一覧</a></p>
+@section('content')
+<main id="main" class="main">
+    <div class="wrap">
+        <p class="margin"><a href="{{ route('knowhow') }}">知っトク情報一覧</a></p>
 
-    <table>
-        <tr>
-            <th>タイトル</th>
-            <th>サムネイル</th>
-            <th>詳細な概要</th>
-            <th>内容</th>
-        </tr>
+        <!-- パンくずリスト  -->
+        <ol class="breadCrumb-001">
+            <li><a href="{{ route('top') }}">ホーム</a></li>
+            <li><a href="{{ route('knowhow') }}">知っトク情報一覧</a></li>
+            <li><a href="{{ route('details') . '?param=' . request()->query('param') }}">カテゴリ詳細</a></li>
+        </ol>
 
-        @foreach($items as $item)
-            <tr>
-                <th>{{ $item->title }}</th>
-                <th>{{ $item->thumbnail }}</th>
-                <th>{{ $item->summary_detail }}</th>
-                <th>{{ $item->content }}</th>
-            </tr>
-        @endforeach
-    </table>
+        <!-- flexbox -->
+
+        <div class="mainInner">
+            <article class="mainContainer">
+                <section class="titleArea">
+                    <!-- タイトルコーナー -->
+                    <h2 class="title">{{ $items[0]->title }}</h2>
+                    <p class="overview">{{ $items[0]->summary_detail }}</p>
+                </section>
+
+                @foreach($items as $item)
+                    <section>
+                        <h3>見出しテストです</h3>
+                        <p>{{ $item->content }}</p>
+                        <h4>小見出しテストです</h4>
+                    </section>
+                @endforeach
+
+                <!-- 一覧を見る -->
+                <div class="linkBtnBox">
+                    <a href="" class="linkBtn">一覧に戻る</a>
+                </div>
+            </article>
+
+            <!-- サイドバー -->
+            <aside class="sidebar">
+                <!-- <nav class="globalNav">辞書</nav> -->
+                <ul>
+                    {{-- pram値の仕様を決めて入力する --}}
+                    <li class="subMenu1 active"><a href="{{ route('details') . '?param=' . 4 }}">開発環境<br>セットアップ</a></li>
+                    <li class="subMenu2"><a href="{{ route('details') . '?param=' . 5 }}">WordPress</a></li>
+                    <li class="subMenu3"><a href="{{ route('details') . '?param=' . 6 }}">おすすめ<br>Webアプリ</a></li>
+                    <li class="subMenu4"><a href="{{ route('details') . '?param=' . 7 }}">おすすめ<br>Webサイト</a></li>
+                    <li class="subMenu5"><a href="{{ route('details') . '?param=' . 8 }}">ショートカット</a></li>
+                    <li class="subMenu6"><a href="{{ route('details') . '?param=' . 9 }}">資格</a></li>
+                    <li class="subMenu7"><a href="{{ route('details') . '?param=' . 10 }}">Chrome拡張機能</a></li>
+                    <li class="subMenu8"><a href="{{ route('details') . '?param=' . 11}}">卒業生の作品</a></li>
+                </ul>
+            </aside>
+        </div>
+    </div>
+</main>
 @endsection
 
 @section('pageJs')
