e237757987c6d1c0c1943ce04c299ec8531dec18
fukuchen62 <fukuchen62@gmail.com>
2023-08-07 20:27:20 +0900

    辞書の管理機能を修正した
    辞書の管理機能を修正した
    
    福島

--- resources/views/cms/cms_vocabulary_list.blade.php
+++ resources/views/cms/cms_vocabulary_list.blade.php
@@ -22,7 +22,7 @@
     </ul>
 
     {{-- 検索条件入力フォーム --}}
-    <form action="{{ route('indexvocabulary') }}" method="get" class="search">
+    <form id="search" action="{{ route('indexvocabulary') }}" method="get" class="search">
         <div>
             検索条件&nbsp;<input type="text" value="{{ $s }}" name="s">
             <input type="submit" value="検索" class="search_btn">
@@ -30,18 +30,21 @@
     </form>
 
     {{-- 言語選択フォーム --}}
-    <form action="{{ route('indexvocabulary') }}" method="get">
+    <form id='lang' action="{{ route('indexvocabulary') }}" method="get">
         <div>
-            言語選択&nbsp;<input type="hidden" name="form_identifier" value="form2">
+            言語選択&nbsp;
+            <input type="hidden" name="form_identifier" value="form2">
+
             <select name="language">
-                <option value="1">HTML</option>
-                <option value="2">CSS</option>
-                <option value="3">JavaScript</option>
-                <option value="4">PHP</option>
-                <option value="5">Python</option>
-                <option value="6">よく使う英単語</option>
+                @foreach ($langlist as $key => $item)
+                    @if ($language_id == $item->id)
+                        <option value="{{ $item->id }}" selected>{{ $item->language_name }}</option>
+                    @else
+                        <option value="{{ $item->id }}">{{ $item->language_name }}</option>
+                    @endif
+                @endforeach
             </select>
-            <input type="submit" value="言語で絞る">
+            <input type="submit" value="言語で絞る" class="search_btn">
         </div>
     </form>
 
@@ -51,9 +54,9 @@
             <th width="5%">言語種別ID</th>
             <th width="10%">英単語(スペル)</th>
             <th width="10%">日本語発音(ルビ)</th>
-            <th width="10%">英語発音記号</th>
+            {{-- <th width="10%">英語発音記号</th> --}}
             <th>意味(プログラミング言語)</th>
-            <th>意味(日本語)</th>
+            {{-- <th>意味(日本語)</th> --}}
             <th>使用例</th>
             <th width="5%">難易度ID</th>
             <th width="10%">修正</th>
@@ -64,9 +67,9 @@
                 <td>{{ $item->language_id }}</td>
                 <td>{{ $item->word_spell }}</td>
                 <td>{{ $item->japanese }}</td>
-                <td>{{ $item->pronunciation }}</td>
+                {{-- <td>{{ $item->pronunciation }}</td> --}}
                 <td>{{ $item->meaning }}</td>
-                <td>{{ $item->notion }}</td>
+                {{-- <td>{{ $item->notion }}</td> --}}
                 <td>{{ $item->usage }}</td>
                 <td>{{ $item->level_id }}</td>
                 <td class="edit"><a href="{{ route('editvocabulary', ['id' => $item->id]) }}">編集</a></td>
