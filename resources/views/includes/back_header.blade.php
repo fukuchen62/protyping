251f7c28497fe0684cf126740cbea23b53ba2a4d
yamakawa00 <139103368+yamakawa00@users.noreply.github.com>
2023-08-10 14:37:03 +0900

    管理画面
    ロゴ、タイトル修正
    
    山川

--- resources/views/includes/back_header.blade.php
+++ resources/views/includes/back_header.blade.php
@@ -2,18 +2,18 @@
     <div class="header container">
         <div class="header-logo">
             <a href="{{ route('top') }}" target="_blank">
-                <img src="{{ asset('assets/images/cat.jpg') }}" alt="Logo画像" class="logo">
+                <img src="{{ asset('assets/images/typecode-logo.svg') }}" alt="Logo画像" class="logo">
             </a>
             <h1 class="page_title">タイプコード管理システム</h1>
         </div>
 
         <div class="login">
             @if (Auth::check())
-                <span>ログイン：{{ $login_user->name }}</span>
+            <span>ログイン：{{ $login_user->name }}</span>
 
-                <a class="logout" href="{{ route('logout') }}">ログアウト</a>
+            <a class="logout" href="{{ route('logout') }}">ログアウト</a>
             @else
-                return redirect()->route('admintop');
+            return redirect()->route('admintop');
             @endif
 
         </div>
